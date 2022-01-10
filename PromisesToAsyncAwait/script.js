5d617aef7574c94755a0a047bc43fc0e67c6c621
saurabh <saurabhj8087445@gmail.com>
2022-01-10 17:02:21 +0530

    add reduce code

--- PromisesToAsyncAwait/script.js
+++ PromisesToAsyncAwait/script.js
@@ -1,24 +1,24 @@
+// promise to async await convert
 function myPromise() {
-    return new Promise(resolve => {
-        setTimeout(() => resolve('resolved'), 5000);
-    })
-
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("resolved"), 5000);
+  });
 }
 
-myPromise().then(v => console.log(v));
+myPromise().then((v) => console.log(v));
 
 async function getValue() {
-    const v = await myPromise();
-    console.log(v);
+  const v = await myPromise();
+  console.log(v);
 }
 
 // array reduce
 const numbers = [1, 2, 3, 4, 5];
 
 const sum = numbers.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
+  return accumulator + currentValue;
 }, 0);
-// accumulator will collect the sum and 
+// accumulator will collect the sum and
 // currentValue will have value of each element at a time.
 
 console.log(sum);
